4f7b5e0e597e6b73656660bd6e96e4dd862c9aac
damenac <david.mendez-acuna@inria.fr>
2015-10-08 15:48:56 +0200

    new results

--- refm-competition-ws/fr.inria.diverse.refm.competition.lopez/test/fr/inria/diverse/refm/competition/lopez/BenchmarkingLopezREFM.java
+++ refm-competition-ws/fr.inria.diverse.refm.competition.lopez/test/fr/inria/diverse/refm/competition/lopez/BenchmarkingLopezREFM.java
@@ -36,8 +36,8 @@ public class BenchmarkingLopezREFM {
 	@Before
 	public void loadScenarios(){
 		synthesizer = new LopezREFM();
-		initialInstance = 19;
-		finalInstance = 27;
+		initialInstance = 1;
+		finalInstance = 18;
 	}
 	
 	@Test
